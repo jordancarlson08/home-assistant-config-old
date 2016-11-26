75bc82192696ef432e3c663b0aa32ffb44119a2e
Jordan Carlson <jcarlson@ancestry.com>
2016-11-26 11:56:43 -0700

    lock state check test

--- apps/lock.py
+++ apps/lock.py
@@ -11,32 +11,17 @@ import appdaemon.appapi as appapi
 
 class LockSync(appapi.AppDaemon):
     def initialize(self):
-        # lock = lock.back_door_locked_15_0
-        # alarm = sensor.back_door_alarm_type_15_0
+
         self.lock = self.args["lock"]
         self.alarm = self.args["alarm"]
 
-        self.log("LockSync: Initialize state listeners")
-        # self.listen_state(self.lock, self.alarm, new='18')
-        # self.listen_state(self.lock, self.alarm, new='21')
-        #
-        # self.listen_state(self.unlock, self.alarm, new='19')
-        # self.listen_state(self.unlock, self.alarm, new='22')
+        self.listen_state(self.sync_lock, self.alarm)
 
-        # self.listen_state(self.lock, "sensor.back_door_alarm_type_15_0")
-        # self.listen_state(self.lock, self.alarm)
-
-        self.listen_state(self.unlock, self.alarm)
-        # self.listen_state(self.unlock, self.alarm)
-
-    def lock(self, entity, attribute, old, new, kwargs):
-        self.log("LockSync: Lock")
-        self.log(old)
-        self.log(new)
-        # self.call_service("lock/lock", entity_id="lock.back_door_locked_15_0")
-
-    def unlock(self, entity, attribute, old, new, kwargs):
+    def sync_lock(self, entity, attribute, old, new, kwargs):
         self.log("LockSync: Unlock")
-        self.log(old)
-        self.log(new)
-        self.call_service("lock/unlock", entity_id=self.lock)
+        self.log("old:" + old)
+        self.log("new:" + new)
+        if new == 18 or new == 21:
+            self.call_service("lock/lock", entity_id=self.lock)
+        if new == 19 or new == 22:
+            self.call_service("lock/unlock", entity_id=self.lock)
