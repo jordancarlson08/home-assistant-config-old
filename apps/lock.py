a0f7a4288bd4bd8a3698f6485ecf8021e14991b7
Jordan Carlson <jcarlson@ancestry.com>
2016-11-26 11:28:37 -0700

    test

--- apps/lock.py
+++ apps/lock.py
@@ -11,6 +11,8 @@ import appdaemon.appapi as appapi
 
 class LockSync(appapi.AppDaemon):
     def initialize(self):
+        # lock = lock.back_door_locked_15_0
+        # alarm = sensor.back_door_alarm_type_15_0
         self.lock = self.args["lock"]
         self.alarm = self.args["alarm"]
 
@@ -21,15 +23,17 @@ class LockSync(appapi.AppDaemon):
         # self.listen_state(self.unlock, self.alarm, new='19')
         # self.listen_state(self.unlock, self.alarm, new='22')
 
-        self.listen_state(self.lock, self.alarm)
-        self.listen_state(self.lock, self.alarm)
+        self.listen_state(self.lock, "sensor.back_door_alarm_type_15_0")
+        # self.listen_state(self.lock, self.alarm)
 
         # self.listen_state(self.unlock, self.alarm)
         # self.listen_state(self.unlock, self.alarm)
 
     def lock(self, entity, attribute, old, new, kwargs):
         self.log("LockSync: Lock")
-        self.call_service("lock.lock", entity_id=self.lock)
+        self.log(old)
+        self.log(new)
+        self.call_service("lock.lock", entity_id="lock.back_door_locked_15_0")
 
     def unlock(self, entity, attribute, old, new, kwargs):
         self.log("LockSync: Unlock")
