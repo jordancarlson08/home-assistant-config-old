a99aee0266b0ba8dc2ef7d21357e3e69ad00b253
Jordan Carlson <jcarlson@ancestry.com>
2016-11-26 10:02:02 -0700

    lock fix

--- apps/lock.py
+++ apps/lock.py
@@ -14,11 +14,11 @@ class LockSync(appapi.AppDaemon):
         self.lock = self.args["lock"]
         self.alarm = self.args["alarm"]
 
-        self.listen_state(self.lock, self.alarm, new="18")
-        self.listen_state(self.lock, self.alarm, new="21")
+        self.listen_state(self.lock, self.alarm, new=18)
+        self.listen_state(self.lock, self.alarm, new=21)
 
-        self.listen_state(self.unlock, self.alarm, new="19")
-        self.listen_state(self.unlock, self.alarm, new="22")
+        self.listen_state(self.unlock, self.alarm, new=19)
+        self.listen_state(self.unlock, self.alarm, new=22)
 
     def lock(self):
         self.call_service("lock.lock", entity_id=self.lock)
