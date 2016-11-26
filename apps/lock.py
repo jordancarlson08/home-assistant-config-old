2b248046ce67faf73118f5d5a04bd17818c3c6e0
Jordan Carlson <jcarlson@ancestry.com>
2016-11-26 12:14:47 -0700

    lock sync cleanup

--- apps/lock.py
+++ apps/lock.py
@@ -5,22 +5,18 @@ import appdaemon.appapi as appapi
 # Lock Sync App
 #
 # Args:
-#   lock
-#   alarm
+#   lock    -- entity id of the lock
+#   alarm   -- entity id of the "lock alarm"
 #
 
 class LockSync(appapi.AppDaemon):
     def initialize(self):
-
         self.lock = self.args["lock"]
         self.alarm = self.args["alarm"]
 
         self.listen_state(self.sync_lock, self.alarm)
 
     def sync_lock(self, entity, attribute, old, new, kwargs):
-        self.log("LockSync: Unlock")
-        self.log("old:" + old)
-        self.log("new:" + new)
         if new == "18" or new == "21":
             self.call_service("lock/lock", entity_id=self.lock)
         if new == "19" or new == "22":
