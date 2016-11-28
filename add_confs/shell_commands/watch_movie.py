25c2420740f5250f5f0892248a7919087eb67562
Jordan Carlson <jcarlson@ancestry.com>
2016-11-27 20:58:32 -0700

    tv first

--- add_confs/shell_commands/watch_movie.py
+++ add_confs/shell_commands/watch_movie.py
@@ -1,8 +1,8 @@
 from codes import Codes
 
 # Turn on TV and sound, open Plex.
-Codes.send_command(body=Codes.roku_home)
 Codes.send_command(body=Codes.tv_power)
+Codes.send_command(body=Codes.roku_home)
 Codes.send_command(body=Codes.sound_power)
 Codes.send_command(body=Codes.roku_home)
 Codes.send_command(body=Codes.roku_home)
