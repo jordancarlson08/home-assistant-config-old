2623b78ad47b863e112bdb7a195c9af2c2364fb9
Jordan Carlson <jcarlson@ancestry.com>
2016-11-27 22:41:48 -0700

    improve watch movie script

--- add_confs/shell_commands/watch_movie.py
+++ add_confs/shell_commands/watch_movie.py
@@ -1,5 +1,6 @@
 from codes import Codes
 
 # Turn on TV and sound, open Plex.
-codes = [Codes.tv_power, Codes.roku_home, Codes.sound_power, Codes.roku_home, Codes.roku_home, Codes.roku_right, Codes.roku_right, Codes.roku_ok]
+# codes = [Codes.tv_power, Codes.roku_home, Codes.sound_power, Codes.roku_home, Codes.roku_home, Codes.roku_right, Codes.roku_right, Codes.roku_ok]
+codes = [Codes.tv_power, Codes.roku_home, Codes.sound_power]
 Codes.send_commands(codes)
