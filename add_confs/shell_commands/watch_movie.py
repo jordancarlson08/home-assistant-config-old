724b452c2448a00793189737b5b42657182a6c0c
Jordan Carlson <jcarlson@ancestry.com>
2016-11-27 20:40:09 -0700

    add watch movie script

---
+++ add_confs/shell_commands/watch_movie.py
@@ -0,0 +1,13 @@
+from codes import Codes
+
+# Turn on TV and sound, open Plex.
+Codes.send_command(body=Codes.roku_home)
+Codes.send_command(body=Codes.tv_power)
+Codes.send_command(body=Codes.sound_power)
+Codes.send_command(body=Codes.roku_home)
+Codes.send_command(body=Codes.roku_home)
+Codes.send_command(body=Codes.roku_right)
+Codes.send_command(body=Codes.roku_right)
+Codes.send_command(body=Codes.roku_ok)
+
+
