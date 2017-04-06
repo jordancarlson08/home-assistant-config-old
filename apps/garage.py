6b5f4ed867d67e4c7f5c96561c9ee595208a2c8a
Jordan Carlson <jcarlson@ancestry.com>
2017-04-06 09:16:58 -0600

    garage notification test

--- apps/garage.py
+++ apps/garage.py
@@ -11,34 +11,28 @@ import datetime
 
 class Garage(appapi.AppDaemon):
     def initialize(self):
-
         self.listen_state(self.notify_garage, "cover.garage_door")
         self.listen_state(self.garage_reminder, "cover.garage_door")
-
-
         time = datetime.datetime.now().time()
         time = time.strftime('%l:%M:%S %p')
+        self.call_service("notify/html5", title="Garage", message="Garage initialized at " + time)
 
-
-        actionData={"tag" : "garage_reminder", "actions": [ {"action": "garage_close", "title": "Close Garage" } ] } 
-
-        self.call_service("notify/html5", title="Garage", message="Garage initialized at " + time, data=actionData)
 
     def notify_garage(self, entity, attribute, old, new, kwargs):
         time = datetime.datetime.now().time()
         time = time.strftime('%l:%M:%S %p')
-
-        self.call_service("notify/html5", title="Garage", message=new + " at " + time)
+        actionData={ "tag" : "garage_" + new }
+        self.call_service("notify/html5", title="Garage", message=new + " at " + time, data=actionData)
 
 
     def garage_reminder(self, entity, attribute, old, new, kwargs):
         if (new == 'open'):
-            # Start a timer
             self.handle = self.run_in(self.notify_garage_open, 300)
         else:
             # if handler isn't null, cancel timer
             self.cancel_timer(self.handle)
 
-    def notify_garage_open(self, kwargs):
 
-        self.call_service("notify/html5", title="Garage", message="Garage open for 5 minutes.")      
+    def notify_garage_open(self, kwargs):
+        actionData={"tag" : "garage_reminder", "actions": [ {"action": "garage_close", "title": "Close Garage" } ] } 
+        self.call_service("notify/html5", title="Garage", message="Garage open for 5 minutes.", data=actionData)      
