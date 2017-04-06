06387241cd86fb05128b3e803f7788fee09d39e7
Jordan Carlson <jcarlson@ancestry.com>
2017-04-06 14:35:58 -0600

    notification action test

--- apps/garage.py
+++ apps/garage.py
@@ -13,9 +13,15 @@ class Garage(appapi.AppDaemon):
     def initialize(self):
         self.listen_state(self.notify_garage, "cover.garage_door")
         self.listen_state(self.garage_reminder, "cover.garage_door")
+        self.listen_event(self.close_garage, event="html5_notification.clicked", action="garage_close")
+
         time = datetime.datetime.now().time()
         time = time.strftime('%l:%M:%S %p')
         self.call_service("notify/html5", title="Garage", message="Garage initialized at " + time)
+
+
+        actionData={"tag" : "garage_reminder", "actions": [ {"action": "garage_close", "title": "Close Garage" } ] } 
+        self.call_service("notify/html5", title="Garage", message="Test action", data=actionData)     
 
 
     def notify_garage(self, entity, attribute, old, new, kwargs):
@@ -36,3 +42,6 @@ class Garage(appapi.AppDaemon):
     def notify_garage_open(self, kwargs):
         actionData={"tag" : "garage_reminder", "actions": [ {"action": "garage_close", "title": "Close Garage" } ] } 
         self.call_service("notify/html5", title="Garage", message="Garage open for 5 minutes.", data=actionData)      
+
+    def close_garage(self, kwargs):
+        self.call_service("cover/close_cover", entity_id="cover.garage_door")
